633046a011cd16fee7cac0c87c7d1198f3be870b
asafonov <me@asafonov.org>
2014-10-24 18:02:19 +0400

    small fixes

--- lib/screenshot.py
+++ lib/screenshot.py
@@ -16,22 +16,22 @@ def screen_gtk(filename):
     pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,sz[0],sz[1])
     pb = pb.get_from_drawable(w,w.get_colormap(),0,0,0,0,sz[0],sz[1])
     if (pb != None):
-        pb.save(filename(),"png")
+        pb.save(filename,"png")
 
 def screen_image_magic(filename):
-    os.system("import -window root "+filename())
+    os.system("import -window root "+filename)
 
 def upload_ftp(filename, config):
     ftp = ftplib.FTP(config('server'))
     ftp.login(config('login'), config('password'))
     ftp.cwd(config('upload_dir'))
-    myfile = open(filename(), 'rb')
-    ftp.storbinary('STOR ' + filename(), myfile)
+    myfile = open(filename, 'rb')
+    ftp.storbinary('STOR ' + filename, myfile)
     myfile.close()
-    os.remove(filename())
-    return config('url_prefix') + filename()
+    os.remove(filename)
+    return config('url_prefix') + filename
 
 def screenshot(screen, upload):
     get_filename = get_filename_from_time(time.time())
-    screen(get_filename)
-    return upload(get_filename, config_default())
+    screen(get_filename())
+    return upload(get_filename(), config_default())
