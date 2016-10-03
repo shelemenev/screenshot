3429313bdff6a2b03c14534b033de9dd9a30405a
asafonov <me@asafonov.org>
2016-10-03 15:13:09 +0200

    python3 is the default

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TMPNAME=`pwd`
 echo cd $TMPNAME > /usr/bin/screenshot
-echo "URL=\`python start.py\`" >> /usr/bin/screenshot
+echo "URL=\`python3 start.py\`" >> /usr/bin/screenshot
 echo "google-chrome-stable \$URL" >> /usr/bin/screenshot
 chmod 755 /usr/bin/screenshot
